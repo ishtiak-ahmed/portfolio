6956db53d51f9b8c71bd50e6998e0eb98700c52d
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-05-08 16:26:03 +0600

    blog section updated and style improved

--- src/Compontents/Blog/Blog.js
+++ src/Compontents/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Blog.css'
 const Blog = () => {
     const blogs = [
         {
@@ -8,14 +8,14 @@ const Blog = () => {
             thumb: 'https://miro.medium.com/max/700/1*Wg2KVuuJVoL1M2O0GqgFGw.jpeg'
         },
         {
-            title: '10 Build in JavaScript Function Junior Developer Should Know',
-            link: 'https://ishtiak-ahmed.medium.com/10-javascript-build-in-function-junior-developer-should-know-814e5beba310',
-            thumb: 'https://miro.medium.com/max/700/1*Wg2KVuuJVoL1M2O0GqgFGw.jpeg'
+            title: 'Beginner way of Solving JavaScript Error, try, catch and debugging',
+            link: 'https://ishtiak-ahmed.medium.com/beginner-way-of-solving-javascript-error-try-catch-and-debugging-668d6e104178',
+            thumb: 'https://miro.medium.com/max/700/0*3OkH_OqrkG5bfof-'
         },
         {
-            title: '10 Build in JavaScript Function Junior Developer Should Know',
-            link: 'https://ishtiak-ahmed.medium.com/10-javascript-build-in-function-junior-developer-should-know-814e5beba310',
-            thumb: 'https://miro.medium.com/max/700/1*Wg2KVuuJVoL1M2O0GqgFGw.jpeg'
+            title: 'Core Fundamental of React And Performance Boosting',
+            link: 'https://ishtiak-ahmed.medium.com/core-fundamental-of-react-and-performance-boosting-ca4524c46ce6',
+            thumb: 'https://miro.medium.com/max/700/0*j6cHNBqxOUbTpouq'
         },
     ]
 
@@ -29,7 +29,9 @@ const Blog = () => {
                             return (
                                 <div className='col-md-4 p-2'>
                                     <div className="blog-item">
-                                        <img className='card-img-top' src={blog.thumb} alt="" />
+                                        <div height='100px' className="img">
+                                            <img className='card-img-top' src={blog.thumb} alt="" />
+                                        </div>
                                         <h5><a href={blog.link} target='_blank' rel='no-referrer'>{blog.title}</a></h5>
                                     </div>
                                 </div>
