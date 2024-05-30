8e231a41f3fce1bfc791398efe867b3097bdf5ed
Ishtiak Ahmed <iahmed1606@gmail.com>
2024-05-30 11:32:52 +0600

    add hyperlink and github, linkedin, email address in about section

--- src/Compontents/About/About.js
+++ src/Compontents/About/About.js
@@ -14,14 +14,29 @@ const About = () => {
               experience in building and maintaining web application.
             </p>
             <p>
-              I've had the opportunity to work at <a href="http://markopolo.ai/" target="_blank">Markopolo.ai</a> and  <a href="https://c-sunshine.com/" target="_blank">C Sunshine</a>,
-              where I honed my skills in front-end. I also have some
+              I've had the opportunity to work at{" "}
+              <a href="http://markopolo.ai/" target="_blank">
+                Markopolo.ai
+              </a>{" "}
+              and{" "}
+              <a href="https://c-sunshine.com/" target="_blank">
+                C Sunshine
+              </a>
+              , where I honed my skills in front-end. I also have some
               opportunities to work on the backend part.
             </p>
             <p>
               Outside of work, I enjoy exploring new technologies and staying
               up-to-date with the latest industry trends. In my free time, I
               love reading books and gardening. I also love sports and games.
+            </p>
+            <p>
+              I am learning and developing some exciting projects since 2019. My{" "}
+              <a href="https://github.com/ishtiak-ahmed" target="_blank">
+                Github
+              </a>{" "}
+              and <a href="https://www.linkedin.com/in/ishtiak-aahmed/" target="_blank">Linkedin</a> profile. You can reach me directly via email.
+              <a href="mailto:ishtiak.aahmed@gmail.com" >ishtiak.aahmed@gmail.com</a>
             </p>
           </div>
           <div className="col-md-3 circle">
