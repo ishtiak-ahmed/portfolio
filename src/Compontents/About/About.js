7db8a635a83404e11fc6be14233271e0d35cf84c
Ishtiak Ahmed <iahmed1606@gmail.com>
2024-05-30 06:21:44 +0600

    update about section information

--- src/Compontents/About/About.js
+++ src/Compontents/About/About.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import Contact from '../Contact/Contact';
-import photo from '../../images/photo.jpg'
+import React from "react";
+import Contact from "../Contact/Contact";
+import photo from "../../images/photo.jpg";
 const About = () => {
-    return (
-        <section style={{ backgroundColor: '#37474F' }} className='text-white py-5'>
-            <h2 className='text-center highlights fw-bold m-3'>About me</h2>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-9">
-                        <p>I am Ishtiak Ahmed, form Joypurhat, Bangladesh. I am a passionate Web Developer. Normally I use <strong>React, Bootstrap, Font Awesome</strong> to build the client side. For Back end I am currently using <strong>Node JS, Express JS, MongoDB</strong>.<br /> </p>
-                        <p>I am open to learn New Technology and Improve my skills.</p>
-                        <p>
-                            I am learning and developing some exciting projects since 2019. My <a href="https://github.com/ishtiak-ahmed" target='_blank' rel='noreferrer'>Github</a> and <a href="https://www.linkedin.com/in/ishtiak-ahmed-1606/" target='_blank' rel='noreferrer'>Linkedin</a> profile. You can reach me directly via email. <span color='#4e47dd'>iahmed1606@gmail.com</span>
-                            <br />
-            Except coding, I also love to read books, solve puzzle, play chess.
+  return (
+    <section style={{ backgroundColor: "#37474F" }} className="text-white py-5">
+      <h2 className="text-center highlights fw-bold m-3">About me</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-9">
+            <p>
+              I am Ishtiak Ahmed, form Joypurhat, Bangladesh. I am a passionate
+              Web Developer and have about three years of professional
+              experience in building and maintaining web application.
             </p>
-                    </div>
-                    <div className="col-md-3 circle">
-                        <img src={photo} className='w-100 rounded' alt="" />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+            <p>
+              I've had the opportunity to work at <a href="http://markopolo.ai/" target="_blank">Markopolo.ai</a> and  <a href="https://c-sunshine.com/" target="_blank">C Sunshine</a>,
+              where I honed my skills in front-end. I also have some
+              opportunities to work on the backend part.
+            </p>
+            <p>
+              Outside of work, I enjoy exploring new technologies and staying
+              up-to-date with the latest industry trends. In my free time, I
+              love reading books and gardening. I also love sports and games.
+            </p>
+          </div>
+          <div className="col-md-3 circle">
+            <img src={photo} className="w-100 rounded" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
